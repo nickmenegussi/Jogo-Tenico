f99ae3b431522d436a997e4e32bd91e1b6b263f1
Nicolas menegussi ramos <106559733+nickmenegussi@users.noreply.github.com>
2023-08-09 11:48:49 -0300

    Update arquivos_uteis.py

--- arquivos_uteis.py
+++ arquivos_uteis.py
@@ -1,10 +1,13 @@
 from pyfiglet  import Figlet as fg
 from termcolor import colored
 
-def apresentar_programa(texto):
+def apresentar_programa(texto, id_caminho):
     texto = fg(font="standard")
     print(colored(texto.renderText(" Bem vindo"), 'blue'))
 
-def digitar_textos_repetitivos(palavra):
-    return print(palavra)
+def textos_repetivos(nome, ):
 
+    if 
+    textofinal_chekpoint_Missao1Caminho1 = f"{nome} (voce) e a filha do comerciante decidem investigar a lenda antiga durante o festival em busca de pistas sobre o tesouro. Voces aprendem com os moradores locais sobre as histórias e mitos que cercam o tesouro escondido. No entanto, as informações são vagas e contraditórias, levando-os a se questionar sobre a veracidade da lenda.\033[m"
+
+    textofinal_chekpoint_Missao1Caminho2 = f"Santiago escolhe ajudar a filha do comerciante a resolver seu problema pessoal, priorizando sua amizade em relação à busca pelo tesouro. Voces passam tempo juntos, discutindo os desafios que ela enfrenta e encontrando soluções juntos. "
